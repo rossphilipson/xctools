4febc1658bb4d2db3cf063399b879adaa588675d
Ross Philipson <philipsonr@ainfosec.com>
2015-12-18 15:26:40 -0500

    [numa] Split Vm and node names

--- numautil/src/numa_config.sh
+++ numautil/src/numa_config.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-VMLIST=(WindowsVM1:0 WindowsVM2:1 ThinVM1:0 ThinVM2:0 ThinVM3:1 ThinVM4:1)
+VMLIST=(Windows7VM1:0 Windows7VM2:0
+Windows7VM3:1 Windows7VM4:1 Network:0
+WinTPC1:0 WinTPC2:0 WinTPC3:0 WinTPC4:0 WinTPC5:0
+WinTPC6:1 WinTPC7:1 WinTPC8:1 WinTPC9:1 WinTPC10:1)
 
 CTON=(`./numautil -c`)
 
-for i in ${CTON[@]}; do
-     echo C-N: $i
-done
-
 for i in ${VMLIST[@]}; do
-     echo Item: $i
+     vm=$(echo $i | cut -d\: -f1)
+     node=$(echo $i | cut -d\: -f2)
+     echo Setting node affinity for $vm to $node
+
 done
