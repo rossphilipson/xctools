cb8f7f933947533939fcb4508d197d6b6df77ce9
Ross Philipson <philipsonr@ainfosec.com>
2016-01-04 16:43:58 -0500

    [numa] Return a vcpus value.

--- numautil/src/numa_config.sh
+++ numautil/src/numa_config.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 VMLIST=(Windows7VM1:0 Windows7VM2:0
-Windows7VM3:1 Windows7VM4:1 Network:0
+Windows7VM3:1 Windows7VM4:1 Domain0:0 Network:0
 WinTPC1:0 WinTPC2:0 WinTPC3:0 WinTPC4:0 WinTPC5:0
 WinTPC6:1 WinTPC7:1 WinTPC8:1 WinTPC9:1 WinTPC10:1)
 
-CTON=(`./numautil -c`)
+#CTON=(`./numautil -c`)
+CTON=(`../test/numautil -c`)
 
 for i in ${VMLIST[@]}; do
      vm=$(echo $i | cut -d\: -f1)
